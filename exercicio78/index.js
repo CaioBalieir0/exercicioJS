5c08de1439e9338dfb8e4f124d7b3bf2f73ad015
CaioBalieir0 <caiobalieiro676@gmail.com>
2023-05-29 03:00:03 -0300

    commit

---
+++ exercicio78/index.js
@@ -0,0 +1,28 @@
+// let div = document.getElementById("div");
+// let btn = document.getElementById("btn");
+// let arr = [];
+
+// btn.addEventListener("click", () => {
+//   let n1 = parseFloat(document.getElementById("inp1").value);
+//   let n2 = parseFloat(document.getElementById("inp2").value);
+
+//   if (n1 > 0 && n2 > 0 && n1 !== n2) {
+//     let menor = n1;
+//     let maior = n2;
+
+//     if (n1 > n2) {
+//       menor = n2;
+//       maior = n1;
+//     }
+
+//     let cont = menor + 1;
+
+//     while (cont < maior) {
+//       if()
+
+//       cont++
+//     }
+//   } else {
+//     alert("Erro");
+//   }
+// });
