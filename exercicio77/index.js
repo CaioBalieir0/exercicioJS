6d3fde5cfa750a98bdcf626ff89e169750203db4
CaioBalieir0 <caiobalieiro676@gmail.com>
2023-05-29 03:43:39 -0300

    first commit

--- exercicio77/index.js
+++ exercicio77/index.js
@@ -1,28 +1,38 @@
-// let div = document.getElementById("div");
-// let btn = document.getElementById("btn");
-// let arr = [];
+let div = document.getElementById("div");
+let btn = document.getElementById("btn");
+let arr = [];
 
-// btn.addEventListener("click", () => {
-//   let n1 = parseFloat(document.getElementById("inp1").value);
-//   let n2 = parseFloat(document.getElementById("inp2").value);
+btn.addEventListener("click", () => {
+  let n1 = parseFloat(document.getElementById("inp1").value);
+  let n2 = parseFloat(document.getElementById("inp2").value);
 
-//   if (n1 > 0 && n2 > 0 && n1 !== n2) {
-//     let menor = n1;
-//     let maior = n2;
+  if (n1 > 0 && n2 > 0 && n1 !== n2) {
+    let menor = n1;
+    let maior = n2;
 
-//     if (n1 > n2) {
-//       menor = n2;
-//       maior = n1;
-//     }
+    if (n1 > n2) {
+      menor = n2;
+      maior = n1;
+    }
 
-//     let cont = menor + 1;
+    menor++;
+    while (menor < maior) {
+      if (menor % 5 == 0) {
+        arr.push(menor);
+      }
+      menor++;
+    }
 
-//     while (cont < maior) {
-//       if()
+    div.innerHTML = `Os números compreendidos múltiplos de 5 são: ${arr.join(
+      " - "
+    )}`;
 
-//       cont++
-//     }
-//   } else {
-//     alert("Erro");
-//   }
-// });
+    let n = "Não existem números mútiplos de 5";
+    if (arr.length == 0) {
+      div.innerHTML = "";
+      div.innerHTML = `${n}`;
+    }
+  } else {
+    alert("ERRO - Números inválidos");
+  }
+});
--- exercicio78/index.js
+++ exercicio78/index.js
@@ -1,28 +1,36 @@
-// let div = document.getElementById("div");
-// let btn = document.getElementById("btn");
-// let arr = [];
+let div = document.getElementById("div");
+let btn = document.getElementById("btn");
+let arr = [];
 
-// btn.addEventListener("click", () => {
-//   let n1 = parseFloat(document.getElementById("inp1").value);
-//   let n2 = parseFloat(document.getElementById("inp2").value);
+btn.addEventListener("click", () => {
+  let n1 = parseFloat(document.getElementById("inp1").value);
+  let n2 = parseFloat(document.getElementById("inp2").value);
 
-//   if (n1 > 0 && n2 > 0 && n1 !== n2) {
-//     let menor = n1;
-//     let maior = n2;
+  if (n1 > 0 && n2 > 0 && n1 !== n2) {
+    let menor = n1;
+    let maior = n2;
 
-//     if (n1 > n2) {
-//       menor = n2;
-//       maior = n1;
-//     }
+    if (n1 > n2) {
+      menor = n2;
+      maior = n1;
+    }
 
-//     let cont = menor + 1;
+    while (maior >= menor) {
+      if (maior % 3 == 0 && maior % 7 == 0) {
+        arr.push(maior);
+      }
+      maior--;
+    }
 
-//     while (cont < maior) {
-//       if()
-
-//       cont++
-//     }
-//   } else {
-//     alert("Erro");
-//   }
-// });
+    let n = "Não existem números múltiplos de 3 e 7";
+    if (arr.length == 0) {
+      div.innerHTML = `${n}`;
+    } else {
+      div.innerHTML = `Os números múltiplos compreendidos entre 3 e 7 são: ${arr.join(
+        " - "
+      )}`;
+    }
+  } else {
+    alert("Erro");
+  }
+});
